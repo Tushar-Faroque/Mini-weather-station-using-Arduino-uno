a7ee59b94bb2ae4e287b454bf028d078490a1198
samiha ferdous <33787926+samiha-ferdous@users.noreply.github.com>
2019-11-18 18:56:44 -0800

    revised

--- water.ino
+++ water.ino
@@ -1,34 +1,47 @@
-int resval=0; //holds the value
-int restpin= A5 //sensor pin used
+/* Arduino Tutorial - Watel Level Sensor 40mm
+   More info: */
+
+const int read = A0; //Sensor AO pin to Arduino pin A0
+int value;          //Variable to store the incomming data
 
 void setup()
 {
-  Serial.begin(9600);//strts the serial console
+  //Begin serial communication
+  Serial.begin(9600);
   
 }
 
 void loop()
 {
-  resval= analogRead(respin); //read data from analog pin and reserve it in resval variable
-
-  if(resval<=100)
-  {
-    Serial.println("Water level:Empty");
+  value = analogRead(read); //Read data from analog pin and store it to value variable
+  
+  if (value<=480){ 
+    Serial.println("Water level: 0mm - Empty!"); 
   }
-  else if(resval>100 && resval<=300)
-  {
-    Serial.println("Water level: Low")
+  else if (value>480 && value<=530){ 
+    Serial.println("Water level: 0mm to 5mm"); 
   }
-
-  else if(resval>300 && resval<=330)
-  {
-    Serial.println("Water level: Medium")
+  else if (value>530 && value<=615){ 
+    Serial.println("Water level: 5mm to 10mm"); 
   }
-
-  else if(resval>330)
-  {
-    Serial.println("Water level: High")
+  else if (value>615 && value<=660){ 
+    Serial.println("Water level: 10mm to 15mm"); 
+  } 
+  else if (value>660 && value<=680){ 
+    Serial.println("Water level: 15mm to 20mm"); 
   }
-
-  delay(1000)
+  else if (value>680 && value<=690){ 
+    Serial.println("Water level: 20mm to 25mm"); 
+  }
+  else if (value>690 && value<=700){ 
+    Serial.println("Water level: 25mm to 30mm"); 
+  }
+  else if (value>700 && value<=705){ 
+    Serial.println("Water level: 30mm to 35mm"); 
+  }
+  else if (value>705){ 
+    Serial.println("Water level: 35mm to 40mm"); 
+  }
+  
+  delay(5000); // Check for new value every 5 sec
 }
