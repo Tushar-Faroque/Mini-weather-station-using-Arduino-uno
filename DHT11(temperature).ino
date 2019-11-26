aba80e2ae31ee27c0d2dee25685bb83d98fc1e71
samiha ferdous <33787926+samiha-ferdous@users.noreply.github.com>
2019-11-26 14:11:25 +0545

    Update DHT11(temperature).ino

--- DHT11(temperature).ino
+++ DHT11(temperature).ino
@@ -1,19 +1,19 @@
-#include <dht.h>
+#include <dht.h> //including the dht library
 
 dht DHT;
 
-#define DHT11_PIN 7
+#define DHT11_PIN 7 //setting the dht at pin 7 in arduino uno
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);    //speed 9600 bits per second
 }
 
 void loop()
 {
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
+  int chk = DHT.read11(DHT11_PIN);// read datas and store it to the variables
+  Serial.print("Temperature = "); //printing the values on the serial monitor
   Serial.println(DHT.temperature);
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
-  delay(1000);
+  delay(1000);       // the arduino waits for a second
 }
